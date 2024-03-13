e8bffd39677634bca50717a4c415dd36e690a2c7
adriansanchezlarrondo <sanchezlarrondoadrian@fpllefia.com>
2024-03-13 20:55:54 +0100

    milisegundos

--- ejercicio2/main.js
+++ ejercicio2/main.js
@@ -1,29 +1,36 @@
 const url = 'https://pokeapi.co/api/v2/pokemon'
 
+// meter la funcion de escucha del boton
+
 async function pokemons() {
     try {
+        const inicioPokemon = new Date().getTime()
         for (let i = 1; i <= 12; i++) {
             let res = await fetch(url + '/' + i)
             res = await res.json()
             await insertaCards(res)
         }
+        const finalPokemon = new Date().getTime()
+        const duracionPokemon = finalPokemon - inicioPokemon
+        console.log('duracionPokemon', duracionPokemon);
+        document.querySelector('#tiempo1').innerHTML = `TIEMPO: ${duracionPokemon} milisegundos`
     } catch (error) {
-        console.error(error);
+        console.error(error)
     }
 }
 pokemons()
 
 // Exercici 2.0
+// Exercici 2.1 Async/await
 async function insertaCards(json){
-    console.log(json);
     document.querySelector('#cards').innerHTML += `
         <div class="col-md-2">
             <div class="card shadow">
-                <img src="${json.sprites.front_default}" class="card-img-top" alt="Bulbasaur">
+                <img src="${json.sprites.front_default}" class="card-img-top" alt="${json.name}">
                 <div class="card-body">
                     <h5 class="card-title text-capitalize">${json.name}</h5>
                     <div class="card-text">ID: ${json.id}</div>
-                    <div class="card-text">Tipo: ${json.name}</div>
+                    <div class="card-text">Tipo: ${json.types.map(type => type.type.name).join(', ')}</div>
                     <div class="card-text">Peso: ${json.weight}</div>
                     <div class="card-text">Altura: ${json.height}</div>
                 </div>
@@ -31,8 +38,6 @@ async function insertaCards(json){
         </div>
     `
 }
-
-// Exercici 2.1 Async/await
 
 // Exercici 2.2 .then/.catch/.finally
 
